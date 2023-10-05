92792732cc69630e3662c830449f59bf6329818e
SirModv <mduke1231@gmail.com>
2023-10-05 16:55:01 -0500

    index.js

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2/promise');
-
+const mysql = require("mysql2/promise");
+// coded wth help from stack overflow 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'employee_tracker',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "employee_tracker",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
